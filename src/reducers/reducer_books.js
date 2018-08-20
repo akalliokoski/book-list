f2e326eaef1581263e8ad2ce450ecb4c8dddbc98
Antti Kalliokoski <antti.a.kalliokoski@gmail.com>
2018-08-20 14:21:46 +0300

    Implement book list based on lectures

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: "Javasacript", pages: 101 },
+        { title: "Harry Potter", pages: 39 },
+        { title: "The Dark Towwer", pages: 80 },
+        { title: "Eloguqnt Ruby", pages: 1 }
+    ]
+}
